f9e5d5bc1cef4aaaec2a6eeb9bd346fa0e7be462
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2016-05-22 08:47:15 -0700

    gitclone.sh initial version
    gitclone.sh initial version: clones in needed git repos for other tools.

---
+++ gitclone.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env 
+
+# tools to clone from git into the bin directory.
+
+# curl-trace/
+
+git clone https://github.com/mathiasbynens/dotfiles.git ~/bin/dotfiles
+git clone https://github.com/barryclark/bashstrap.git ~/bin/bashstrap
+git clone https://github.com/facebook/PathPicker.git ~/bin/PathPicker
+git clone https://github.com/wickett/curl-trace.git ~/bin/curl-trace
+
+echo "gitclone.sh: Installation complete."
