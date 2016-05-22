1e104f2c68db47fa1ea52e2000a211e77ea4e479
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2016-05-22 09:24:55 -0700

    added git-extra-commands
    added git-extra-commands

--- gitclone.sh
+++ gitclone.sh
@@ -2,11 +2,10 @@
 
 # tools to clone from git into the bin directory.
 
-# curl-trace/
-
 git clone https://github.com/mathiasbynens/dotfiles.git ~/bin/dotfiles
 git clone https://github.com/barryclark/bashstrap.git ~/bin/bashstrap
 git clone https://github.com/facebook/PathPicker.git ~/bin/PathPicker
 git clone https://github.com/wickett/curl-trace.git ~/bin/curl-trace
+git clone https://github.com/unixorn/git-extra-commands.git ~/bin/git-extra-commands
 
 echo "gitclone.sh: Installation complete."
