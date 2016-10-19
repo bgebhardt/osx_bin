0318b01c2d2229da788a491d8ca6adac42068957
Bryan Gebhardt <bryan@Bryans-iMac-2.local>
2016-10-19 11:34:43 -0700

    Made it into a runnable script with proper cask installs Added notes regarding all the apps I use and ones I used to use

--- setup/brew-cask.sh
+++ setup/brew-cask.sh
@@ -19,160 +19,161 @@ brew install cask
 
 # install apps
 
-## TODO: add brew install cask before each cask
-## TODO: need to filtuer out which ones are from the App Store now too
+## TODO: need to filter out which ones are from the App Store now too
 ## TODO: figure out which versions of apps I have licenses for and load those.
 
 ## Top Must Installs
-1password
-fastscripts
-iterm2
-flux
-omnifocus
-mailplane
-dropbox
-google-drive
-wd-my-cloud # also WD Sync Installer.app
+brew cask install 1password
+brew cask install fastscripts
+brew cask install iterm2
+brew cask install flux
+brew cask install omnifocus
+brew cask install mailplane
+brew cask install dropbox
+brew cask install google-drive
+brew cask install wd-my-cloud # also WD Sync Installer.app
 # Mail Plugin Manager.app - manually install this one or create cask
-sizzlingkeys
-textmate
-timing
+brew cask install sizzlingkeys
+brew cask install textmate
+brew cask install timing
+brew cask install divvy
 
 ## Productivity/Office
-adium
-adobe-reader
-amadeus-pro
-audacity
-busycal
-busycontacts
-fullcontact
-elmedia-player
-evernote
+brew cask install adium
+brew cask install adobe-reader
+brew cask install amadeus-pro
+brew cask install audacity
+brew cask install busycal
+brew cask install busycontacts
+brew cask install fullcontact
+brew cask install elmedia-player
+brew cask install evernote
 #fantastical
-firefox
-google-chrome
-google-earth
-intensify-pro
-kiwi
-kindle
-send-to-kindle
-markdown-service-tools
-qlmarkdown
-metanota
-textexpander
-omnigraffle
-omnioutliner
-pdf-converter-master
-pdfpen
-slack
-skype
-soulver
-logitech-myharmony
-spotify
-vlc
-
+brew cask install firefox
+brew cask install google-chrome
+brew cask install flash-player
+brew cask install google-earth
+brew cask install music-manager
+brew cask install intensify-pro
+brew cask install kiwi
+brew cask install kindle
+brew cask install send-to-kindle
+brew cask install markdown-service-tools
+brew cask install qlmarkdown
+# brew cask install metanota. # install cask broken
+brew cask install textexpander
+brew cask install omnigraffle
+#brew cask install omnioutliner # install cask broken - bad download url
+brew cask install pdf-converter-master
+brew cask install pdfpen
+brew cask install slack
+brew cask install skype
+brew cask install soulver
+brew cask install logitech-myharmony
+brew cask install spotify
+brew cask install vlc
+brew cask install hype # web animation program
+# brew cask install lego-mindstorms-ev3 # not currently using
 
 ## Utilities
-1clipboard
-amazon-drive
-amazon-music
-bartender
-beamer
-boom
-brightness
-caffeine
-carbon-copy-cloner
-crashplan
-cronnix
-crossover
-daisydisk
-default-folder-x
-disk-drill
-disk-sensei
+brew cask install 1clipboard
+brew cask install amazon-drive
+#brew cask install amazon-music # cask broken; app not working
+brew cask install bartender
+brew cask install beamer
+brew cask install boom
+brew cask install brightness
+brew cask install caffeine
+brew cask install carbon-copy-cloner
+brew cask install crashplan
+brew cask install cronnix
+brew cask install crossover
+brew cask install daisydisk
+brew cask install default-folder-x
+brew cask install disk-drill
+brew cask install disk-sensei
+brew cask install appdelete
 # disk-inventory-x
 # omnidisksweeper
-divvy
 #dropbox-encore
 #fluid
-forklift
-grandperspective
-growlnotify
-little-snitch
-gemini
-macpilot
-macupdate-desktop
-mactracker
-rcdefaultapp
-plex-media-server
-popclip
-superduper
-supersync
-wallpaper-wizard
-iphoto-library-manager
-imazing
-
+brew cask install forklift
+brew cask install grandperspective
+brew cask install growlnotify
+# brew cask install notifyr # for some reason this prefpane not working on 10.12; claims Bluetooth 4.0 not supported
+brew cask install little-snitch
+brew cask install gemini
+brew cask install macpilot
+brew cask install macupdate-desktop
+brew cask install mactracker
+brew cask install rcdefaultapp
+brew cask install plex-media-server
+brew cask install popclip
+brew cask install superduper
+brew cask install supersync
+brew cask install wallpaper-wizard
+brew cask install iphoto-library-manager
+brew cask install imazing
 
 ## Development apps
-atom
-diffmerge
-aptanastudio
-aqua-data-studio
-aquamacs
-base
-codekit
-coderunner
-dash
-boot2docker
-couchbase-server-community
-deltawalker
-espresso
-github-desktop
+brew cask install atom
+brew cask install diffmerge
+brew cask install aptanastudio
+brew cask install aqua-data-studio
+brew cask install aquamacs
+brew cask install base
+brew cask install codekit
+brew cask install coderunner
+brew cask install dash
+brew cask install boot2docker
+brew cask install couchbase-server-community
+brew cask install deltawalker
+brew cask install espresso
+brew cask install github-desktop
+brew cask install xquartz
 #the-escapers-flux
 #quicklook-json
-quickjson
-key-codes
-mamp
-navicat-for-mysql
-pycharm
-rstudio
-rapidweaver
-virtualbox                # VirtualBox is a powerful x86 and AMD64/Intel64 virtualization product
-virtualbox-extension-pack # VirtualBox Extension Pack
-vagrant                   # Vagrant is a tool for building and distributing development environments.
+brew cask install quickjson
+brew cask install key-codes
+brew cask install mamp
+brew cask install navicat-for-mysql
+brew cask install pycharm
+brew cask install rstudio
+brew cask install rapidweaver
+brew cask install virtualbox                # VirtualBox is a powerful x86 and AMD64/Intel64 virtualization product
+brew cask install virtualbox-extension-pack # VirtualBox Extension Pack
+brew cask install vagrant                   # Vagrant is a tool for building and distributing development environments.
 
-visual-studio-code
-sts
-kitematic
-sequel-pro
-xquartz
-java
-java7
-yourkit-java-profiler
+brew cask install visual-studio-code
+brew cask install sts
+brew cask install kitematic
+brew cask install sequel-pro
+brew cask install java
+# brew cask install yourkit-java-profiler # cask broken
 
-- qlcolorcode         # Preview source code files with syntax highlighting
-- qlstephen           # Preview plain text files without a file extension. Example: README, CHANGELOG, etc.
-- qlmarkdown          # Preview Markdown files
-- quicklook-json      # Preview JSON files
-- qlprettypatch       # Preview .patch files
-- quicklook-csv       # Preview CSV files
-- betterzipql         # Preview archives
-- webpquicklook       # Preview WebP images
+brew cask install qlcolorcode         # Preview source code files with syntax highlighting
+brew cask install qlstephen           # Preview plain text files without a file extension. Example: README, CHANGELOG, etc.
+brew cask install qlmarkdown          # Preview Markdown files
+brew cask install quicklook-json      # Preview JSON files
+brew cask install qlprettypatch       # Preview .patch files
+brew cask install quicklook-csv       # Preview CSV files
+brew cask install betterzipql         # Preview archives
+brew cask install webpquicklook       # Preview WebP images
 
-- facter        # Facter gathers basic facts about systems. such as hardware, network settings, OS type and more.
-- sublime-text3 # Sublime Text is a sophisticated text editor for code, markup and prose.
-- gitup         # Visualization Tool for Git
-- kaleidoscope  # File and Folder comparison tool
-
+#brew cask install facter        # cask broken # Facter gathers basic facts about systems. such as hardware, network settings, OS type and more.
+#brew cask install sublime-text3 # cask broken # Sublime Text is a sophisticated text editor for code, markup and prose.
+brew cask install gitup         # Visualization Tool for Git
+brew cask install kaleidoscope  # File and Folder comparison tool
 
 
 # Games
-gog-galaxy
-steam
-steamcmd
-teamspeak-client
-minecraft-server
-minecraft
-battle-net
+brew cask install gog-galaxy
+brew cask install steam
+brew cask install steamcmd
+brew cask install teamspeak-client
+brew cask install minecraft-server
+brew cask install minecraft
+brew cask install battle-net
 
 
 # clean up cached cask files
