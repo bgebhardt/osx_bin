f131f33b5a6306e08349ee28381bd34674578228
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2021-03-14 13:44:59 -0700

    initial windows packages installable by choco

---
+++ setup/choco.ps1
@@ -0,0 +1,87 @@
+# inspired by https://gist.github.com/ddieppa/b44b58a12c200f917ef2b3a7b6d97fc5
+
+# base
+#choco install 1password # didn't work
+choco install chocolateygui
+choco install powertoys
+
+# NiftyWindows adds to the built-in features of the window manger to increase ease of use.
+# http://www.enovatic.org/products/niftywindows/
+choco install niftywindows
+
+choco install netcfpowertoys
+choco install keytweak
+
+# file access
+choco install googledrive
+choco install google-backup-and-sync
+#choco install icloud - didn't work - install from Microsoft Windows store
+choco install onedrive
+
+# main apps
+choco install itunes
+choco install zoom
+choco install discord
+
+# browsers
+#choco install -y firefox
+choco install -y googlechrome
+
+# games
+choco install steam-client
+choco install epicgameslauncher
+choco install goggalaxy
+
+# utils
+# choco install -y 7zip.install
+# choco install -y conemu
+# choco install -y ditto
+# choco install -y greenshot
+# choco install -y adobereader
+# choco install -y vlc
+# choco install -y youtube-dl
+# choco install -y aria2
+# choco install -y ffmpeg
+# choco install -y keepass.install
+# choco install -y rufus
+# choco install -y irfanview
+# choco install -y teracopy
+# choco install -y foxitreader --ia '/MERGETASKS="!desktopicon,!setdefaultreader,!displayinbrowser /COMPONENTS=*pdfviewer,*ffse,*installprint,*ffaddin,*ffspellcheck,!connectedpdf"'
+# choco install -y advanced-renamer.install
+
+# chocolatey packages creation
+choco install -y ussf
+choco install -y checksum
+
+# wsl
+choco install -y wsl 
+choco install -y wsl2 
+# choco install wsl-ubuntu-1804
+
+# development
+## general
+choco install -y nodejs
+choco install -y git
+choco install -y firacode 
+choco install -y cascadiacodepl
+choco install -y cascadiacode
+choco install -y postman
+choco install -y git-fork
+choco install -y powershell-preview
+choco install -y sql-server-management-studio
+
+## vscode
+choco install -y vscode
+
+## visual studio 2017 community
+# choco install -y visualstudio2017community 
+# ### visual studio 2017 workloads
+# choco install visualstudio2017-workload-azure
+# choco install visualstudio2017-workload-manageddesktop
+# choco install visualstudio2017-workload-data
+# choco install visualstudio2017-workload-netcoretools
+# choco install visualstudio2017-workload-netcrossplat
+# choco install visualstudio2017-workload-netweb
+# choco install visualstudio2017-workload-node
+# choco install visualstudio2017-workload-universal
+
