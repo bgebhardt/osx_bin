324866da35591116f090fbab070cd8833a9e27ed
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2022-08-11 00:23:48 -0700

    Trimmed list of apps installed

--- setup/mas.sh
+++ setup/mas.sh
@@ -7,65 +7,64 @@
 
 brew install mas # Install App Store command line tools https://github.com/mas-cli/mas
 
-mas install 567704457 # OmniPopLite (3.2)
+#mas install 567704457 # OmniPopLite (3.2)
 mas install 409183694 # Keynote (7.0)
-mas install 456362093 # MuteMyMic (1.10)
-mas install 484388250 # Numi (1.7.7)
+#mas install 456362093 # MuteMyMic (1.10)
+#mas install 484388250 # Numi (1.7.7)
 mas install 784801555 # Microsoft OneNote (15.27.1)
 mas install 823766827 # OneDrive (17.3.6518)
 mas install 560846814 # PDF Converter Master (4.0.0)
 mas install 896450579 # Textual (6.0.2)
 mas install 453114608 # JSON Helper (1.07)
-mas install 682658836 # GarageBand (10.1.2)
-mas install 488536386 # Location Helper (1.02)
-mas install 700757313 # Halloween Fonts (4.0)
+#mas install 682658836 # GarageBand (10.1.2)
+#mas install 488536386 # Location Helper (1.02)
+#mas install 700757313 # Halloween Fonts (4.0)
 mas install 883878097 # Server (5.2)
 mas install 431511738 # Timing (1.7.4)
 mas install 509148961 # SMARTReporter (3.1.11)
 mas install 465965038 # Markdown Pro (1.0.9)
 mas install 607997198 # Marko (2.1)
 mas install 586862299 # Duplicate Cleaner For iPhoto (1.13)
-mas install 497799835 # Xcode (8.0)
+#mas install 497799835 # Xcode (8.0)
 mas install 409203825 # Numbers (4.0)
 mas install 491365225 # OneClickdigital (1.12)
 mas install 409201541 # Pages (6.0)
-mas install 413965349 # Soulver (2.6.0)
+mas install 1508732804 # Soulver 3 (3.6)
+# mas install 413965349 # Soulver (2.6.0)
 mas install 408981434 # iMovie (10.1.2)
 mas install 594444151 # PDF to JPG (4.0)
-mas install 555935825 # Themes for Keynote Free (2.2)
+#mas install 555935825 # Themes for Keynote Free (2.2)
 mas install 929188617 # Timeline 3D (5.1.2)
-mas install 803453959 # Slack (2.0.3)
 mas install 715394237 # Condense (1.61)
 mas install 935700987 # Snapselect (1.3.0)
 mas install 435003921 # Fantastical (1.3.18)
 mas install 615916400 # BrowseShot (1.0)
-mas install 435003921 # Fantastical
-mas install 555935825 # Themes for Keynote Free
-mas install 511648940 # Metanota
-mas install 406056744 # Evernote
-mas install 412980789 # Full Deck Solitaire
-mas install 692867256 # Simplenote
+#mas install 511648940 # Metanota
+#mas install 406056744 # Evernote
+#mas install 412980789 # Full Deck Solitaire
+#mas install 692867256 # Simplenote
 mas install 410801088 # Wallpaper Wizard
-mas install 426397771 # SQLVue
+#mas install 426397771 # SQLVue
 mas install 456624497 # Brightness Slider
 mas install 445189367 # PopClip
-mas install 883878097 # Server
-mas install 576421334 # Converto
-mas install 467939042 # Growl
-mas install 760246983 # Free Fonts - Christmas Collection
-mas install 512556651 # Solve
+#mas install 883878097 # Server
+#mas install 576421334 # Converto
+#mas install 467939042 # Growl
+#mas install 760246983 # Free Fonts - Christmas Collection
+#mas install 512556651 # Solve
 mas install 414568915 # Key Codes
-mas install 528459889 # TuneSpan
-mas install 525517698 # TextSoapMenu-MAS
+#mas install 528459889 # TuneSpan
+#mas install 525517698 # TextSoapMenu-MAS
 mas install 512464723 # Alinof Timer
 mas install 593913958 # Expresso - text calculator
-mas install 409789998 # Twitter
+#mas install 409789998 # Twitter
 mas install 525912054 # WiFi Signal
-mas install 415492014 # Inpaint 
-mas install 503729945 # SizzlingKeys (5.1.4)
-mas install 715768417 # Microsoft Remote Desktop (8.0.27246)
+#mas install 415492014 # Inpaint 
+#mas install 503729945 # SizzlingKeys (5.1.4)
 mas install 549083868 # Display Menu (2.2.2)
 mas install 688211836 # EasyRes (1.1.1)
+mas install 1274495053  # Microsoft To Do  ## NOT WORKING; TODO try to fix
+mas install 1295203466 # Microsoft Remote Desktop
 
 # mas install 521464274 # Farensius Desktop # seems to no longer be available
 
@@ -75,6 +74,7 @@ mas install 688211836 # EasyRes (1.1.1)
 # 503729945 SizzlingKeys
 # 1094748271 FullContact
 # 405399194 Kindle
+# mas install 803453959 # Slack (2.0.3) ## NOT WORKING; TODO try to fix
 
 ## Apple apps
 
