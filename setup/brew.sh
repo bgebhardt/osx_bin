48b5f32e60336b6fcd3f24f2860eb3d90fe9b4d7
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2022-11-18 20:57:28 -0800

    removed aircrack-ng

--- setup/brew.sh
+++ setup/brew.sh
@@ -59,7 +59,7 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
+#brew install aircrack-ng # gets flagged as a malicious file so removing.
 brew install bfg
 brew install binutils
 brew install binwalk
