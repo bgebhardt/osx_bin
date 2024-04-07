d6a9550e07bc8b67741e423c52aed89b82315e33
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2024-04-06 19:53:35 -0700

    Initial script to open all Office caches and log directories to clean them up

---
+++ scripts/delete-office-file-cache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# [Clear the Office cache - Office Add-ins | Microsoft Learn](https://learn.microsoft.com/en-us/office/dev/add-ins/testing/clear-cache#clear-the-office-cache-on-mac)
+# [Comprehensive Guide To Clear Office Cache on Mac (2023)](https://www.imymac.com/powermymac/clear-office-cache-mac.html#:~:text=Find%20%26%20Delete%20Microsoft%20Office%20Cache%20via%20Finder%2FTerminal,directories%2C%20use%20command-A.%20Click%20command%20delete%20after%20that.)
+# [How to clear MS Office cache manually and automatically](https://macpaw.com/how-to/clear-office-cache)
+# Try CleanMyMax X?
+
+# Open directories containing caches or logs
+open ~/Library/Containers/com.microsoft.Excel/Data/Library/Application\ Support/Microsoft/AppData/Microsoft/Office/16.0/
+open ~/Library/Containers/com.microsoft.Excel/Data/Library/Caches
+open ~/Library/Containers/com.microsoft.Excel/Data/Library/Logs/Diagnostics/EXCEL
+
+open ~/Library/Containers/com.microsoft.Word/Data/Library/Application\ Support/Microsoft/AppData/Microsoft/Office/16.0/
+open ~/Library/Containers/com.microsoft.Word/Data/Library/Caches
+open ~/Library/Containers/com.microsoft.Word/Data/Library/Logs/Diagnostics/WORD
+
+open ~/Library/Containers/com.microsoft.Powerpoint/Data/Library/Application\ Support/Microsoft/AppData/Microsoft/Office/16.0/
+open ~/Library/Containers/com.microsoft.Powerpoint/Data/Library/Caches
+open ~/Library/Containers/com.microsoft.Powerpoint/Data/Library/Logs/Diagnostics/POWERPOINT
+
+
+
