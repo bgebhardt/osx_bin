ea6c6f39d5513cbc86fb9e7e91b1e7a47b22fff4
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2024-12-27 21:26:25 -0800

    got a fragile version working that will download all installers

--- setup/appstore/install-other-apps.sh
+++ setup/appstore/install-other-apps.sh
@@ -17,33 +17,48 @@
 # * Quick expose - no cask; same software maker [Quick Exposé: A New Way to Use Mission Control and App Exposé on macOS • MacPlus Software](https://noteifyapp.com/quick-expose/) 
 # * Rapidweaver - download 9.1.1 from [RapidWeaver Release Notes](https://www.realmacsoftware.com/rapidweaver/releasenotes/) or [direct link](https://dl.devant.io/v1/3c53887f-427a-4af7-9144-ee16178c62f4/21049/RapidWeaver.zip)
 
+# Function to download and install an app from a URL, detecting if it is a zip file
+download_app() {
+    local url=$1
+    local app_name=$2
+    local downloadName="$(basename "$url")"
+    local download_dir="$HOME/Downloads"
+    local localFilePath="$download_dir/$downloadName"
+
+    echo "Downloading $app_name..."
+    echo "  from $url"
+    echo "  to $localFilePath"
+
+    #curl -L -o ~/Downloads/FreeFileSync_13.9_macOS.zip https://freefilesync.org/download/FreeFileSync_13.9_macOS.zip
+    # curl -L -o "$localFilePath" "$url"
+    open "$url" # opens the URL in the default browser and downloads the file to Downloads. Very fragile though.
+}
 
 # Function to download and install an app from a URL, detecting if it is a zip file
 install_app() {
     local url=$1
     local app_name=$2
-
-    echo "Downloading $app_name..."
-
+    local downloadName="$(basename "$url")"
     local download_dir="$HOME/Downloads"
+    local localFilePath="$download_dir/$downloadName"
 
-    curl -L -o $download_dir/$app_name $(basename $url)
-
-    if [[ $url == *.zip ]]; then
+        if [[ $url == *.zip ]]; then
         echo "Unzipping $app_name..."
-        unzip -q $download_dir/$app_name.zip -d $download_dir/$app_name
+        #unzip -q "$localFilePath" -d "$download_dir/$app_name"
+        unzip "$localFilePath"
 
         echo "Installing $app_name..."
-        cp -r $download_dir/$app_name/*.app /Applications/
+        # copy app to /Applications or run the pkg installer
+        #cp -r $download_dir/$app_name/*.app /Applications/
     else
         echo "Mounting $app_name.dmg..."
-        hdiutil attach $download_dir/$app_name.dmg -nobrowse -quiet
+        hdiutil attach "$localFilePath" #-quiet
 
         echo "Installing $app_name..."
-        cp -r /Volumes/$app_name/*.app /Applications/
+        #cp -r /Volumes/$app_name/*.app /Applications/
 
         echo "Cleaning up..."
-        hdiutil detach /Volumes/$app_name -quiet
+        hdiutil detach "/Volumes/$app_name" #-quiet
     fi
 
     #echo "Cleaning up..."
@@ -55,14 +70,15 @@ install_app() {
 # List of apps to install
 apps=(
     # "https://freefilesync.org/download.php FreeFileSync" # go to this link for latest version
-    "https://freefilesync.org/download/FreeFileSync_13.9_macOS.zip" FreeFileSync # hardcoded version for now
+    # hardcoded version for now
+    "https://freefilesync.org/download/FreeFileSync_13.9_macOS.zip FreeFileSync"
     "https://github.com/alyssaxuu/later/raw/master/Later.dmg Later"
     "https://www.drbuho.com/download/buhocleaner.dmg BuhoCleaner"
     "https://wifiradar.app/download WiFiRadar"
     "https://jsonwizard.app/download JSONWizard"
     "https://seense.com/the_clock/updateapp/the_clock.zip TheClock"
     "https://dl.devant.io/v1/3c53887f-427a-4af7-9144-ee16178c62f4/21049/RapidWeaver.zip RapidWeaver"
-    
+        
     # https://skylum.com/account/my-software
     "https://skylum.com/download/luminar-neo-m1-paid" LuminarNeo
 
@@ -77,14 +93,17 @@ apps=(
 # two apps to test with
 test_apps=(
     # "https://freefilesync.org/download.php FreeFileSync" # go to this link for latest version
-    "https://freefilesync.org/download/FreeFileSync_13.9_macOS.zip" FreeFileSync # hardcoded version for 
+    # hardcoded version for now
+    "https://freefilesync.org/download/FreeFileSync_13.9_macOS.zip FreeFileSync"
     "https://jsonwizard.app/download JSONWizard"
 )
 
 # Install each app
+#for app in "${test_apps[@]}"; do
 for app in "${apps[@]}"; do
     IFS=' ' read -r url name <<< "$app"
-    install_app $url $name
+    download_app $url $name
+    #install_app $url $name
 done
 
 echo "All apps installed successfully!"
