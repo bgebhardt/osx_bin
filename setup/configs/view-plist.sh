58cccf896f9a35bc909a7fd7e62f5e19bac5a6bc
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2024-12-27 19:27:11 -0800

    added option to look up preference file by app name (will work for some apps) added usage

--- setup/configs/view-plist.sh
+++ setup/configs/view-plist.sh
@@ -1,16 +1,72 @@
 #!/bin/bash
 # view plist in xml
 
-# Paths to the plist files
-plist1_path="$1"
+# Function to display usage information
+usage() {
+    echo "Usage: $0 [-a app_name] [plist_path]"
+    echo "  -a app_name   Specify the application name to find its plist file"
+    echo "  plist_path    Path to the plist file to be processed"
+    exit 1
+}
+
+# Check if no arguments are passed
+if [ $# -eq 0 ]; then 
+    echo "No arguments provided" 
+    usage
+fi
+
+# Initialize variables
+plist_path=""
+app_name=""
+
+# Function to find the preference file for a given app name
+find_app_plist() {
+    app_name="$1"
+    app_id=$(/usr/bin/osascript -e "id of app \"$app_name\"")
+
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to get app ID for $app_name"
+        exit 1
+    fi
+    
+    #plist_path=$(find ~/Library/Preferences -name "*${app_name}*.plist" 2>/dev/null | head -n 1)
+    #echo "App $app_name has and id of $app_id" # for debugging
+    local result="$HOME/Library/Preferences/${app_id}.plist"
+    echo "$result"    
+}
+
+# Parse options
+while getopts "a:" opt; do
+    case $opt in
+        a)
+            app_name=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
+plist_path=$(find_app_plist "$app_name")
+
+# set the plist path to the last remaining argument
+for arg in "$@"; do 
+    #echo "Remaining argument: $arg"
+    plist_path="$arg"
+done
+
+echo "Processing file: $plist_path"
 
 # Check if a valid plist file is passed in
-if [ ! -f "$plist1_path" ]; then
+if [ ! -f "$plist_path" ]; then
     echo "Error: File not found!"
     exit 1
 fi
 
-if [[ "$plist1_path" != *.plist ]]; then
+if [[ "$plist_path" != *.plist ]]; then
     echo "Error: Not a plist file!"
     exit 1
 fi
@@ -20,12 +76,12 @@ fi
 #plutil -convert xml1 -o "${plist1_path}.xml" "$plist1_path"
 
 # Save the output to a file in the same directory as where script was run
-output_path="$(pwd)/$(basename "${plist1_path}").xml"
+output_path="$(pwd)/$(basename "${plist_path}").xml"
 
 # Print out where the file is written
 echo "Output file written to: $output_path"
 
-plutil -convert xml1 -o "$output_path" "$plist1_path"
+plutil -convert xml1 -o "$output_path" "$plist_path"
 
 # open in default editor (preferring code if it is there)
 # this is a quick hack because "code -w" for some reason didn't work for the EDITOR clause. Likely a bug I need to fix.
