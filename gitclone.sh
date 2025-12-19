b707fd787d5f447cb89c7e9500d36f81a1c9b9a4
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2025-12-19 10:47:31 -0800

    added full brew path in case haven't reloaded environment yet

--- gitclone.sh
+++ gitclone.sh
@@ -4,7 +4,7 @@
 # now using Git CLI as authentication works better
 
 # install the gh cli in case its not there.
-brew install gh
+/opt/homebrew/bin/brew install gh
 
 gh repo clone https://github.com/mathiasbynens/dotfiles.git ~/bin/dotfiles
 gh repo clone https://github.com/barryclark/bashstrap.git ~/bin/bashstrap
