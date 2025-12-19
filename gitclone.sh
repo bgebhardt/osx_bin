7bd9609c6888d8fc943201608df66f932e63f1be
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2025-12-19 10:34:35 -0800

    add install of gh tool

--- gitclone.sh
+++ gitclone.sh
@@ -3,6 +3,9 @@
 # tools to clone from git into the bin directory.
 # now using Git CLI as authentication works better
 
+# install the gh cli in case its not there.
+brew install gh
+
 gh repo clone https://github.com/mathiasbynens/dotfiles.git ~/bin/dotfiles
 gh repo clone https://github.com/barryclark/bashstrap.git ~/bin/bashstrap
 gh repo clone https://github.com/facebook/PathPicker.git ~/bin/PathPicker
